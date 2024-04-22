fdbbeed68d101844e1dd4d148c77c01114d1680a
Areeba Farhan1234 <areebakhan1454771@gmail.com>
2024-04-23 02:01:12 +0500

    change

--- src/Component/Hooks.jsx
+++ src/Component/Hooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Hooks = () => {
   let a = 4;
@@ -10,18 +10,45 @@ const Hooks = () => {
 
   let [num, setNum] = useState(0);
   let [name, setName] = useState("");
+  let count = useRef();
+  count.current = 0;
+  console.log(count.current)
+
+  // useEffect(() => {
+  //   console.log("changes occured!");
+  // });
+
+//   useEffect(() => {
+//     console.log("changes website methods!");
+//   }, []);
+ 
+// useEffect(() => {
+//     console.log("num change methods!");
+//   }, [num]);
+
+ 
+useEffect(() => {
+//     setNum(++num);
+count.current = count.current +1;
+    console.log("num change");
+ });
+
   return (
     <div>
       <h1>Learning Hooks</h1>
       <h2>useState</h2>
       <p>The value of a is : {a}</p>
-      <p>The value of num is : {num}</p>
-      <button onClick={() => increment()}>increment heloo</button>
+      {/* <p>The value of num is : {num}</p> */}
+      <button onClick={() => increment()}>increment</button>
       <br />
       <input type='text' name='' id='' placeholder='enter value' onChange={(e) => {setName(e.target.value); }}/>
       <p>Name : {name}</p>
+
+      <h2>useEffect</h2>
+      <h2>useRef</h2>
+
     </div>
-  )
+  );
 }
 
 export default Hooks
